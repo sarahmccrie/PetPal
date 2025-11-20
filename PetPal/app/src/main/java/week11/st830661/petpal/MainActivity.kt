b357f3e0ca6cf19e4456670edb70787ef5f153a0
Sarah McCrie <sarah.mccrie@gmail.com>
2025-11-20 13:58:36 -0500

    Added logout button for testing

--- PetPal/app/src/main/java/week11/st830661/petpal/MainActivity.kt
+++ PetPal/app/src/main/java/week11/st830661/petpal/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +21,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import week11.st830661.petpal.login.LoginScreen
 import week11.st830661.petpal.ui.theme.PetPalTheme
 
@@ -62,4 +71,28 @@ fun MainScreen(
         text = "Hello Android!",
         modifier = modifier
     )
+    Scaffold { innerPadding ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            // Logout row (top-right)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(onClick = onLogout) {
+                    Text("Logout")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+            
+        }
+    }
 }
