45a98b5faeead8debae03e6777b5bce82b38032d
Cameron <camwilson345@gmail.com>
2025-11-21 13:09:52 -0500

    Updated MedicalRecord object

---
+++ PetPal/app/src/main/java/week11/st830661/petpal/view/login/AddVaccination.kt
@@ -0,0 +1,18 @@
+package week11.st830661.petpal.view.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddVaccination() {
+    Column(modifier = Modifier.padding(10.dp)) {
+        IconButton(onClick = {
+            ->
+
+        }) { }
+    }
+}
